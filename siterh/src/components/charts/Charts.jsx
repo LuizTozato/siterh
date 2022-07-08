72783be0f4cea6f1021a629b7ceac4ea9fa07683
Luiz Felipe Neves Tozato <lfntozato@gmail.com>
2022-07-08 19:25:02 -0300

    Pie chart example created

--- siterh/src/components/charts/Charts.jsx
+++ siterh/src/components/charts/Charts.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import Main from "../template/Main";
-
+import {Chart} from "react-google-charts"
 
 const Charts = () => {
 
-    
+    const data = [
+        ["Fabricante","Vendas"],
+        ["Android", 10],
+        ["IOS", 30],
+        ["Linux", 25],
+        ["Windows", 100],
+        ["Mac", 6]
+    ]
+
     return(
         <Main title="Dados e estatísticas">
 
             <div>Hellow world</div>
+            <Chart 
+                chartType="PieChart" 
+                data={data} 
+                width={"100%"} 
+                height={"400px"}>
+            </Chart>
 
         </Main>
 
